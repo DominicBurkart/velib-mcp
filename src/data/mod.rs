b70e60abe7611afe49d278a4f22c2da5bbdfd13e
Dominic Burkart <dominicburkart@gmail.com>
2026-03-28 09:47:18 +0100

    wire up cache_tests module in data/mod.rs

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -2,5 +2,8 @@ pub mod cache;
 pub mod client;
 pub mod retry;
 
+#[cfg(test)]
+mod cache_tests;
+
 pub use client::VelibDataClient;
 pub use retry::{RetryConfig, RetryPolicy, RetryStrategy, RetryableHttpClient};
